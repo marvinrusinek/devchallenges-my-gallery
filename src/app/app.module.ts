88a46379532f9360104b39c5cc41c5c8ea34eddc
Marvin Rusinek <marvinrusinek@gmail.com>
2021-04-21 14:53:18 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './my-gallery/containers/header/header.component';
+import { MainComponent } from './my-gallery/containers/main/main.component';
+import { FooterComponent } from './my-gallery/containers/footer/footer.component';
+import { ContainerComponent } from './my-gallery/containers/container/container.component';
+import { LazyImgDirective } from './my-gallery/containers/main/lazy-img.directive';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'my-gallery',
+    pathMatch: 'full'
+  },
+  {
+    path: 'my-gallery',
+    loadChildren: () =>
+      import('./my-gallery/routing/my-gallery-routing.module').then(m => m.MyGalleryRoutingModule)
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    ContainerComponent,
+    LazyImgDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
